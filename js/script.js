d48d932ada53721ec23cb8d5c554938d5172b405
bangsya <syafiiskadaku363@gmail.com>
2025-05-08 20:39:55 +0700

    Membuat animasi pada javascript

--- js/script.js
+++ js/script.js
@@ -7,5 +7,21 @@ container.addEventListener('click', function(e){
     // console.log(e.target);
     if (e.target.classList.contains('thumbs')){
         hero.src = e.target.getAttribute('src');
+        hero.classList.add('animate-fade');
+        setTimeout(function(){
+            hero.classList.remove('animate-fade');
+        }, 500);
+
+        thumbs.forEach(function(el){
+            el.classList.remove('active');
+            el.parentElement.classList.remove('shadow-md');
+            el.parentElement.classList.remove('rounded-xl');
+        });
+
+        e.target.parentElement.classList.add('shadow-md');
+        e.target.parentElement.classList.add('rounded-xl');
+        e.target.parentElement.classList.add('shadow-cyan-400');
+
+        e.target.classList.add('active');
     }
 });
